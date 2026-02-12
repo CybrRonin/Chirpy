834101d27e75871072467eb46eba0934d6859121
CybrRonin <APStories@gmail.com>
2026-02-12 15:13:01 -0500

    Initial server setup

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	const port = "8080"
+
+	serveMux := http.NewServeMux()
+	srv := http.Server{
+		Addr:    ":" + port,
+		Handler: serveMux,
+	}
+
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(srv.ListenAndServe())
+}
