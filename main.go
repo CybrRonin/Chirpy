666fc00f5f87e5299459e5b88e29de2e845b9165
CybrRonin <APStories@gmail.com>
2026-02-13 11:07:43 -0500

    added some basic HTTP method routing

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"sync/atomic"
@@ -23,10 +22,11 @@ func main() {
 		fileserverHits: atomic.Int32{},
 	}
 	mux := http.NewServeMux()
-	mux.Handle(filepathApp+"/", apiCfg.middlewareMetricsInc(http.StripPrefix(filepathApp, http.FileServer(http.Dir(filepathRoot)))))
-	mux.HandleFunc(filepathReadiness, handlerReadiness)
-	mux.HandleFunc(filepathMetrics, apiCfg.handlerMetrics)
-	mux.HandleFunc(filepathReset, apiCfg.handlerReset)
+	fsHandler := apiCfg.middlewareMetricsInc(http.StripPrefix(filepathApp, http.FileServer(http.Dir(filepathRoot))))
+	mux.Handle(filepathApp+"/", fsHandler)
+	mux.HandleFunc("GET "+filepathReadiness, handlerReadiness)
+	mux.HandleFunc("GET "+filepathMetrics, apiCfg.handlerMetrics)
+	mux.HandleFunc("POST "+filepathReset, apiCfg.handlerReset)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -35,18 +35,4 @@ func main() {
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
-}
-
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		cfg.fileserverHits.Add(1)
-		next.ServeHTTP(w, req)
-	})
-}
-
-func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, req *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	msg := fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())
-	w.Write([]byte(msg))
 }
